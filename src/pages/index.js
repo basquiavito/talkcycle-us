c79e2b715904335e8c76061bc12cdd28bad43953
Richard G <richardg@richards-imac.mynetworksettings.com>
2023-04-24 08:52:28 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
  <meta name="Description" content="Talkcycle.net — Take your Spanish skills to the next level with Talkcycle's online Spanish course for adults. Our self-paced beginner's course includes easy online Spanish lessons and conversation classes through Zoom, designed to help you become fluent. Submit your practice conversations and receive expert feedback from our instructors, plus one live session a week for assistance and support. Choose the best online Spanish course  and start speaking Spanish like a pro with Talkcycle today!  "/>
  <meta property="og:url" content="https://talkcycle.net/"/>
  <meta property="og:type" content="homepage"/>
- <meta property="og:title" content='Talkcycle: Online Spanish Course  | Hooperos'/>
+ <meta property="og:title" content='Talkcycle: Online Spanish Course '/>
  <meta property="og:description" content="Talkcycle.net — Take your Spanish skills to the next level with Talkcycle's online Spanish course for adults. Our self-paced beginner's course includes easy online Spanish lessons and conversation classes through Zoom, designed to help you become fluent. Submit your practice conversations and receive expert feedback from our instructors, plus one live session a week for assistance and support. Choose the best online Spanish course  and start speaking Spanish like a pro with Talkcycle today!   "/>
  <meta property="og:image" content=''></meta>
  
