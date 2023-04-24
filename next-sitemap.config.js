4fa1bc1149a869e66177a98981fc8de93d7e390a
Richard G <richardg@richards-imac.mynetworksettings.com>
2023-04-24 10:12:02 -0400

    sitempa

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const config = {
+    siteUrl: 'https://www.talkcycle.net',
+    generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+    // Other available options..
+  }
+ 
+module.exports = config;
