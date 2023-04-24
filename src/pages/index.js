e1f9a0c1eea0a80bbaa262b212d3dd8d867543da
Richard G <richardg@richards-imac.mynetworksettings.com>
2023-04-24 08:50:24 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,19 +12,19 @@ export default function Home() {
     <Head>
  
  <meta charSet="utf-8"/>
- <title>Hooperos: Basketball Scoring, Moves, Countermoves, Urban Culture | Hooperos</title>
- <meta name="Description" content="Hooperos.com — is home to the biggest source of NBA basketball signature moves and countermoves collection designed to enhance user performace and provide insight into the world of basketball scoring.  "/>
- <meta property="og:url" content="https://www.hooperos.com/"/>
+ <title>Talkcycle</title>
+ <meta name="Description" content="Talkcycle.net — Take your Spanish skills to the next level with Talkcycle's online Spanish course for adults. Our self-paced beginner's course includes easy online Spanish lessons and conversation classes through Zoom, designed to help you become fluent. Submit your practice conversations and receive expert feedback from our instructors, plus one live session a week for assistance and support. Choose the best online Spanish course  and start speaking Spanish like a pro with Talkcycle today!  "/>
+ <meta property="og:url" content="https://talkcycle.net/"/>
  <meta property="og:type" content="article"/>
- <meta property="og:title" content='Hooperos: Basketball Scoring, Moves, Countermoves, Urban Culture | Hooperos'/>
- <meta property="og:description" content='Hooperos.com — is home to the biggest source of NBA basketball moves and countermoves collection designed to enhance user performace and provide insight into the world of basketball scoring.  '/>
+ <meta property="og:title" content='Talkcycle: Online Spanish Course  | Hooperos'/>
+ <meta property="og:description" content="Talkcycle.net — Take your Spanish skills to the next level with Talkcycle's online Spanish course for adults. Our self-paced beginner's course includes easy online Spanish lessons and conversation classes through Zoom, designed to help you become fluent. Submit your practice conversations and receive expert feedback from our instructors, plus one live session a week for assistance and support. Choose the best online Spanish course  and start speaking Spanish like a pro with Talkcycle today!   "/>
  <meta property="og:image" content='https://res.cloudinary.com/www-hoopscript-com/image/upload/v1680128008/assets/logoInSvg.svg'></meta>
  
- <link rel="icon" type="image/svg" href="/Favi.png"/>
+ <link rel="icon" type="image/svg" href=""/>
  
  
  
- <link rel="canonical" href='https://www.hooperos.com/' content="index,follow"/>
+ <link rel="canonical" href='https://www.talkcycle.net/' content="index,follow"/>
  
  
  
@@ -33,19 +33,19 @@ export default function Home() {
  
  <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
   <meta http-equiv="content-language" content="en"></meta>
- <meta name="copyright" content="Copyright (c) Hooperos 2023"></meta>
- <meta name="author" content="Hooperos"></meta>
+ <meta name="copyright" content="Copyright (c) Talkcycle 2023"></meta>
+ <meta name="author" content="Talkcycle"></meta>
  <meta name="content-type" content="homepage"></meta>
  
   
  <meta name="twitter:card" content="summary_large_image"></meta>
- <meta name="twitter:creator" content="@hooperos_us"></meta>
+ <meta name="twitter:creator" content="@talkcycle"></meta>
  <meta name="twitter:title" content='Home'/> 
- <meta name="twitter:description" content="Talkcycle.com — Take your Spanish skills to the next level with Talkcycle's online Spanish course for adults. Our self-paced beginner's course includes easy online Spanish lessons and conversation classes through Zoom, designed to help you become fluent. Submit your practice conversations and receive expert feedback from our instructors, plus one live session a week for assistance and support. Choose the best online Spanish course  and start speaking Spanish like a pro with Talkcycle today!   "></meta>
- <meta name="article:author" content='Hooperos'></meta>
- <meta property="twitter:image" content='/Favi.png'/>
- <meta property="twitter:site" content="@hooperos_us"></meta>
- <meta name="twitter:url" content="https://www.hooperos.com/">
+ <meta name="twitter:description" content="Talkcycle.net — Take your Spanish skills to the next level with Talkcycle's online Spanish course for adults. Our self-paced beginner's course includes easy online Spanish lessons and conversation classes through Zoom, designed to help you become fluent. Submit your practice conversations and receive expert feedback from our instructors, plus one live session a week for assistance and support. Choose the best online Spanish course  and start speaking Spanish like a pro with Talkcycle today!   "></meta>
+ <meta name="article:author" content='Talkcycle'></meta>
+ <meta property="twitter:image" content=''/>
+ <meta property="twitter:site" content="@talkcycle_us"></meta>
+ <meta name="twitter:url" content="https://www.talkcycle.net/">
  
  
  
@@ -55,14 +55,14 @@ export default function Home() {
  <meta name="robots" content="index, follow, max-image-preview:large"/>
   <meta name="googlebot" content="all" />
   <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"></meta>
-  <meta name="Description" content="Hooperos "/>
+  <meta name="Description" content="Talkcycle "/>
   <meta name="google-site-verification" content="3vPt_83gjgDlwNpSwqVFOgiUWnIP-sBphFQVu_wS6q0"></meta>
  
-  <meta property="og:url" content="https://www.hooperos.com/"/>
+  <meta property="og:url" content="https://www.talkcycle.net/"/>
  <meta property="og:locale" content="en_US"/>
  
- <meta property="fb:app_id" content="908303963313109"/>
- <meta property="og:site_name" content="Hooperos"/>
+ <meta property="fb:app_id" content=""/>
+ <meta property="og:site_name" content="Talkcycle"/>
  
  <meta name="keywords" content='online spanish course for adults, online spanish beginners course, online spanish conversation classes, online spanish lessons easy, best online spanish course for high school students, online spanish courses to become fluent, online spanish classes zoom
 ' ></meta>
