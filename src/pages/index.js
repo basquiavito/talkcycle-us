d76bc4cf4e4281d46a1926aeefcb0e17ab06c46a
Richard G <richardg@richards-imac.mynetworksettings.com>
2023-04-24 09:49:57 -0400

    add google tag manager

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
   <meta name="googlebot" content="all" />
   <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"></meta>
   <meta name="Description" content="Talkcycle "/>
-  <meta name="google-site-verification" content="3vPt_83gjgDlwNpSwqVFOgiUWnIP-sBphFQVu_wS6q0"></meta>
+  <meta name="google-site-verification" content=""></meta>
  
   <meta property="og:url" content="https://www.talkcycle.net/"/>
  <meta property="og:locale" content="en_US"/>
@@ -66,21 +66,44 @@ export default function Home() {
  
  <meta name="keywords" content='online spanish course for adults, online spanish beginners course, online spanish conversation classes, online spanish lessons easy, best online spanish course for high school students, online spanish courses to become fluent, online spanish classes zoom
 ' ></meta>
-   <Script src="https://www.googletagmanager.com/gtag/js?id=G-ZJHY29V7WF"   strategy='afterInteractive'   /> 
 
-  <Script
-  id="gta-tags" strategy='afterInteractive'>
- {`
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', 'G-ZJHY29V7WF');
- `}
- </Script>
+
       </Head>
       <main >
       <h1>Talkcycle</h1>
       </main>
+
+
+
+
+
+
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-ZJHY29V7WF"   strategy='afterInteractive'   /> 
+
+<Script
+id="gta-tags" strategy='afterInteractive'>
+{`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-ZJHY29V7WF');
+`}
+</Script>
+
+
+
+ 
+ 
+ <Script id="google-analytics" strategy='afterInteractive'>
+  {`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ 
+ ga('create', 'G-ZJHY29V7WF', 'auto');
+ ga('send', 'pageview');`}
+ </Script>
+   {/* GA end */}
     </>
   )
 }
