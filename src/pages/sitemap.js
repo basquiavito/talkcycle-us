90f7c98dccc13ac3fbba77d15c80027e8bb4175e
Richard G <richardg@richards-imac.mynetworksettings.com>
2023-05-01 00:34:55 -0400

    ok

--- src/pages/sitemap.js
+++ src/pages/sitemap.js
@@ -10,9 +10,12 @@ export default function sitemap(){
 
         <h2><span>2023</span></h2>
         <h3><span>April</span></h3>
-   <p><Link href="https://www.talkcycle.net/articles/Spanish-practice-with-the-verb-gustar"><span>Spanish Practice With The Verb: Gustar</span></Link></p>  
+   <p><Link href="https://www.talkcycle.net/articles/Spanish-practice-with-the-verb-gustar"><span>Article: Spanish Practice With The Verb: Gustar</span></Link></p>  
 
-     <p><Link href="https://www.talkcycle.net/articles/Spanish-Practice-Word-of-the-Day-Cariño"><span>Spanish Practice Word Of The Day: Cariño</span></Link></p>
+     <p><Link href="https://www.talkcycle.net/articles/Spanish-Practice-Word-of-the-Day-Cariño"><span>Article:  Spanish Practice Word Of The Day: Cariño</span></Link></p>
+
+     <p><Link href="https://www.talkcycle.net/listen/spanish-practice-adjetivo-con-mi-caballo"><span>Listen: Spanish Practice adjectives con mi caballo</span></Link></p>
+
      <h3><span>May</span></h3>
     </div>
     
@@ -37,6 +40,11 @@ export default function sitemap(){
         align-item: center;
         margin-bottom: 2.25rem;
 
+    }
+
+
+  span {
+        text-transform: capitalize;
     }
     
     `}</style>
